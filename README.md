47c6bc6584812ece155d52853ec7c0296e784500
Tanujasontakke <sontakke21.tanuja@gmail.com>
2025-04-15 11:30:46 -0700

    Initial commit - basic app for git useage purpose

---
+++ README.md
@@ -0,0 +1,12 @@
+# Git Task Tracker (POC)
+
+A simple Python CLI task manager built to demonstrate Git skills:
+- Git init, branching, merging
+- Pull requests, commit history, tagging
+- .gitignore usage
+
+## Run
+```bash
+python main.py
+```
+
---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+from utils import load_tasks, save_tasks
+
+def show_menu():
+    print("\nTask Tracker")
+    print("1. View Tasks")
+    print("2. Add Task")
+    print("3. Exit")
+
+if __name__ == "__main__":
+    tasks = load_tasks()
+
+    while True:
+        show_menu()
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+        elif choice == '2':
+            new_task = input("Enter new task: ")
+            tasks.append(new_task)
+            save_tasks(tasks)
+        elif choice == '3':
+            break
+        else:
+            print("Invalid option.")
